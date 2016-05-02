489b8f6467c428d8192081d303f9c4f9dd7d26e8
Hermes David Junior <hmdavidjunior@gmail.com>
2016-05-02 09:18:16 -0400

    Version with bootstrap style

---
+++ react_code_1.1 - bootstrap/src/main.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var ReactDOM = require('react-dom')
+var ListManager = require('./components/ListManager.jsx');
+
+
+var manager1 = ReactDOM.render(<ListManager title="My Ingredients 1" className="listManager" />, document.getElementById('ingredients_1'));
+
+manager1.addItem('Bread');
+manager1.addItem('Catchup');
+
+var manager2 = ReactDOM.render(<ListManager title="My Ingredients 2" className="listManager" />, document.getElementById('ingredients_2'));
+
+manager2.addItem('Bread');
+manager2.addItem('Catchup');
+
+/*
+var obj = ReactDOM.render(
+    <div >
+        <ListManager title="My Ingredients 1" className="listManager" />
+        <ListManager title="My Ingredients 2" className="listManager" />
+    </div>, 
+    document.getElementById('ingredients_1')
+);
+
+console.log(obj);
+*/
